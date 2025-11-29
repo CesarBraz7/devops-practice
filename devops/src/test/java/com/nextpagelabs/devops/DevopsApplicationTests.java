1cec8792afbb5fa6808fef53c691b9c5287a62e2
César <cesardsbraz@gmail.com>
2025-11-29 09:04:03 -0300

    init: initialize devops project with Maven and Spring Boot setup

---
+++ devops/src/test/java/com/nextpagelabs/devops/DevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextpagelabs.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
